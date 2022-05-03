b87a7bd861471a4a91a05f0dd0144e1b6dda7063
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-03 18:13:01 -0400

    added button

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  NativeSyntheticEvent,
+  NativeTouchEvent,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function App() {
+  const handleClick = (ev: NativeSyntheticEvent<NativeTouchEvent>) => {
+    console.log('you clicked')
+  };
   return (
     <View style={styles.container}>
-      <Text>First react native app!</Text>
+      <Text>my First react native app!</Text>
+      <Button title="Click me" onPress={handleClick} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
