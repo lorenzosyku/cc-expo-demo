7d978fd7a20222acd3f3c6f32433e436b7c57c26
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-03 18:20:49 -0400

    refactoring

--- App.tsx
+++ App.tsx
@@ -1,31 +1,15 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  Button,
-  NativeSyntheticEvent,
-  NativeTouchEvent,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+
+
+import { NavigationContainer } from "@react-navigation/native";
+import Home from "./screens/Home";
 
 export default function App() {
-  const handleClick = (ev: NativeSyntheticEvent<NativeTouchEvent>) => {
-    console.log('you clicked')
-  };
+  
   return (
-    <View style={styles.container}>
-      <Text>my First react native app!</Text>
-      <Button title="Click me" onPress={handleClick} />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Home/>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
---
+++ screens/Home.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import { NativeSyntheticEvent, NativeTouchEvent } from "react-native";
+
+const Home = () => {
+  const handleClick = (ev: NativeSyntheticEvent<NativeTouchEvent>) => {
+    console.log("you clicked");
+  };
+  return (
+    <View>
+      <Text>this is the home screen</Text>
+      <View style={styles.container}>
+        <Text>my First react native app!</Text>
+        <Button title="Click me" onPress={handleClick} />
+      </View>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
