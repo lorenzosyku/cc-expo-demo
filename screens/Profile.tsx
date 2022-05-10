feacfbb58f02fc224f4659a76475a66e2fe610ba
lorenzosyku <lorenzo.naomi1920@gmail.com>
2022-05-10 18:25:36 -0400

    trying out stylings

--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,32 +1,33 @@
 import { useNavigation } from "@react-navigation/core";
 import React, { useEffect, useLayoutEffect, useState } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import { NavigationScreenProp } from "../types";
 
 const Profile = () => {
   const navigation = useNavigation<NavigationScreenProp>();
   const [dadjoke, setDadJoke] = useState<string>();
 
-  useEffect(() => {
-    const fetchJoke = async () => {
-      const res = await fetch("https://api.chucknorris.io/jokes/random");
-      const data = await res.json();
-      setDadJoke(data.value);
-    };
+  const fetchJoke = async () => {
+    const res = await fetch("https://api.chucknorris.io/jokes/random");
+    const data = await res.json();
+    setDadJoke(data.value);
+  };
 
+  useEffect(() => {
     fetchJoke();
   }, []);
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      title: "Welcome to the Profile Screen",
+      title: "Jokes",
       headerTitleAlign: "center",
     });
   }, []);
 
   return (
     <View style={styles.profile}>
-      <Text>The dad joke of the day is:</Text>
+      <Text style={styles.content}>The dad joke of the day is:</Text>
+      <Button title="Refresh" onPress={fetchJoke} />
       <Text>{dadjoke}</Text>
     </View>
   );
@@ -38,7 +39,11 @@ const styles = StyleSheet.create({
   profile: {
     flex: 1,
     backgroundColor: "#fff",
+    padding: 10,
+    margin: 10,
     alignItems: "center",
     justifyContent: "center",
   },
+  content:{
+  }
 });
